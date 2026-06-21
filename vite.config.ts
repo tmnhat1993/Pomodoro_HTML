2fcb492245fe031e32f7f16446832f0f81796450
nhat <tmnhat1993@gmail.com>
2026-06-21 21:54:42 +0700

    Update Pages base for Pomodoro repo

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/Podoromo_HTML/' : '/'
+  base: command === 'build' ? '/Pomodoro_HTML/' : '/'
 }));
